fa090ec8323bfe67cb40953ef7ddb559c3fc8da0
canderolon <candeerolonn@gmail.com>
2025-05-23 11:28:04 -0300

    .

--- backend/model/inscription_model.go
+++ backend/model/inscription_model.go
@@ -4,11 +4,10 @@ type InscriptionModel struct {
 	ID              int       `gorm:"primaryKey"`        //PK
 	User            UserModel `gorm:"foreignKey:UserID"` //FK
 	UserID          int
-	DateInscription string `gorm:"type:date;not null"` //Date of inscription
-	Active          bool   `gorm:"default:true"`       //Active status
-
-	Activity   ActivityModel `gorm:"foreignKey:ActivityID"` //FK
-	ActivityID int
+	DateInscription string        `gorm:"type:date;not null"`    //Date of inscription
+	Active          bool          `gorm:"default:true"`          //Active status
+	Activity        ActivityModel `gorm:"foreignKey:ActivityID"` //FK
+	ActivityID      int
 }
 
 type Inscriptions []InscriptionModel
